5d87b76880834d0dd7dae286476325d8adb97766
sulagna07 <51054997+sulagna07@users.noreply.github.com>
2019-10-17 02:35:18 +0530

    file modified

--- EasyCRMApi/EasyCRMApi/Properties/Startup.cs
+++ EasyCRMApi/EasyCRMApi/Properties/Startup.cs
@@ -138,5 +138,10 @@ namespace TestCaveSepApi
 /*
 https://docs.microsoft.com/en-us/aspnet/core/security/authorization/limitingidentitybyscheme?view=aspnetcore-3.0&tabs=aspnetcore2x
 https://medium.com/agilix/asp-net-core-supporting-multiple-authorization-6502eb79f934
-
+https://docs.microsoft.com/en-us/aspnet/core/security/anti-request-forgery?view=aspnetcore-3.0
+https://github.com/aspnet/AspNetCore.Docs/blob/master/aspnetcore/security/anti-request-forgery/sample/AngularSample/Startup.cs
+https://www.c-sharpcorner.com/article/preventing-csrf-attacks-in-asp-net-core-2-0/
+https://stackoverflow.com/questions/42227254/how-to-prevent-csrf-xsrf-in-asp-net-core-mvc-app-web-api-controller
+https://dzone.com/articles/secure-net-core-applications-from-csrf-attack-net
+http://www.dotnet-stuff.com/tutorials/aspnet-core/preventing-cross-site-request-forgery-xsrf-csrf-atacks-in-asp-net-core
 */
