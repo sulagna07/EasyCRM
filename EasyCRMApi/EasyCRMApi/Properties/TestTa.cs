997747f4b21e12c5eee3ff2dc840c0072813ab11
sulagna07 <51054997+sulagna07@users.noreply.github.com>
2019-12-03 01:03:18 +0530

    Add files via upload

---
+++ EasyCRMApi/EasyCRMApi/Properties/TestTa.cs
@@ -0,0 +1,100 @@
+=============================================
+services.AddAuthentication(options =>
+{
+	options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddOpenIdConnect(options =>
+{
+	
+	options.Authority = Configuration["Okta:Domain"] + "/oauth2/default";
+	options.RequireHttpsMetadata = false; //true
+	options.ClientId = Configuration["Okta:ClientId"];
+	options.ClientSecret = Configuration["Okta:ClientSecret"];
+	options.CallbackPath = new PathString("/authorization-code/callback");
+	options.ResponseType = OpenIdConnectResponseType.CodeIdToken;
+	options.GetClaimsFromUserInfoEndpoint = true;
+	options.Scope.Add("openid");
+	options.Scope.Add("profile");
+	options.Scope.Add("email");
+	options.SaveTokens = true;
+	options.TokenValidationParameters = new TokenValidationParameters
+	{
+		NameClaimType = "name",
+		RoleClaimType = "groups",
+		ValidateIssuer = true
+	};
+});
+===============================================
+services.AddAuthentication(options =>
+{
+	// If an authentication cookie is present, use it to get authentication information
+	options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+	// If authentication is required, and no cookie is present, use Okta (configured below) to sign in
+	options.DefaultChallengeScheme =  "Okta"; //OktaDefaults.ApiAuthenticationScheme;
+})
+.AddCookie() // cookie authentication middleware first
+.AddOAuth("Okta", options =>
+{
+	// Oauth authentication middleware is second
+
+	var oktaDomain = Configuration.GetValue<string>("Okta:Domain");
+
+	// When a user needs to sign in, they will be redirected to the authorize endpoint
+	options.AuthorizationEndpoint = $"{oktaDomain}/oauth2/default/v1/authorize";
+
+	// Okta's OAuth server is OpenID compliant, so request the standard openid
+	// scopes when redirecting to the authorization endpoint
+	options.Scope.Add("openid");
+	options.Scope.Add("profile");
+	options.Scope.Add("email");
+
+	// After the user signs in, an authorization code will be sent to a callback
+	// in this app. The OAuth middleware will intercept it
+	options.CallbackPath = new PathString("/authorization-code/callback");
+
+	// The OAuth middleware will send the ClientId, ClientSecret, and the
+	// authorization code to the token endpoint, and get an access token in return
+	options.ClientId = Configuration.GetValue<string>("Okta:ClientId");
+	options.ClientSecret = Configuration.GetValue<string>("Okta:ClientSecret");
+	options.TokenEndpoint = $"{oktaDomain}/oauth2/default/v1/token";
+
+	// Below we call the userinfo endpoint to get information about the user
+	options.UserInformationEndpoint = $"{oktaDomain}/oauth2/default/v1/userinfo";
+
+	// Describe how to map the user info we receive to user claims
+	options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "sub");
+	options.ClaimActions.MapJsonKey(ClaimTypes.Name, "given_name");
+	options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+
+	options.Events = new OAuthEvents
+	{
+		OnCreatingTicket = async context =>
+		{
+			// Get user info from the userinfo endpoint and use it to populate user claims
+			var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+			request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+			var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+			response.EnsureSuccessStatusCode();
+
+			var user = JsonSerializer.Deserialize<dynamic>(await response.Content.ReadAsStringAsync());
+
+			context.RunClaimActions(user);
+		}
+	};
+});
+===================================================================================
+"Okta": {
+    "ClientId": "0oaohmhl1639uYtFL0h7",
+    "ClientSecret": "KJeENRP-5WaltNJp6yqrF_MwyZA7sTISWgdK4rHg",
+    "Domain": "https://dev-198806.oktapreview.com"
+
+}
+====================================================================================
+https://developer.okta.com/blog/2018/04/18/authorization-in-your-aspnet-mvc-4-application
